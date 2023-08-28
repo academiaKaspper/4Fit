f839c8a450fb857595d56a0f030fa9f0a51561af
Vinícius Diniz <105671764+ViniciusDX@users.noreply.github.com>
2023-08-28 19:44:48 -0300

    crud aluno
    crud aluno

---
+++ 4fit/src/main/java/com/fit/domain/dtos/AlunoDTO.java
@@ -0,0 +1,134 @@
+package com.fit.domain.dtos;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fit.domain.Aluno;
+import com.fit.domain.enums.Perfil;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.br.CPF;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class AlunoDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    protected Integer id;
+    @NotNull(message = "O campo NOME é requerido")
+    protected String nome;
+    @CPF
+    @NotNull(message = "O campo CPF é requerido")
+    protected String cpf;
+    @NotNull(message = "O campo DATA DE NASCIMENTO é requerido")
+    protected String dataNascimento;
+    protected String telefone;
+    @NotNull(message = "O campo EMAIL é requerido")
+    protected String email;
+    @NotNull(message = "O campo SENHA é requerido")
+    protected String senha;
+    protected String tipo;
+    protected Set<Integer> perfis = new HashSet<>();
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    protected LocalDate dataCriacao = LocalDate.now();
+
+    public AlunoDTO() {
+        super();
+        addPerfil(Perfil.ALUNO);
+    }
+
+    public AlunoDTO(Aluno obj) {
+        this.id = obj.getId();
+        this.nome = obj.getNome();
+        this.cpf = obj.getCpf();
+        this.dataNascimento = obj.getDataNascimento();
+        this.telefone = obj.getTelefone();
+        this.email = obj.getEmail();
+        this.senha = obj.getSenha();
+        this.tipo = obj.getTipo();
+        this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+        this.dataCriacao = obj.getDataCriacao();
+        addPerfil(Perfil.ALUNO);
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Set<Perfil> getPerfis() {
+        return perfis.stream().map(x -> Perfil.toEnum(x)).collect(Collectors.toSet());
+    }
+
+    public void addPerfil(Perfil perfil) {
+        this.perfis.add(perfil.getCodigo());
+    }
+
+    public LocalDate getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDate dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+}
