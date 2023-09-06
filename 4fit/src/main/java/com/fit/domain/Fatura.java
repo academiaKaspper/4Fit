4ab72e7a56d959c8d5c10d6f450e9c5776166555
Eron Moraes <eron.moraes7@gmail.com>
2023-09-05 23:19:31 -0300

    documentation

--- 4fit/src/main/java/com/fit/domain/Fatura.java
+++ 4fit/src/main/java/com/fit/domain/Fatura.java
@@ -6,21 +6,22 @@ import jakarta.persistence.*;
 
 import java.time.LocalDate;
 import java.time.YearMonth;
+
 @Entity
 public class Fatura {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-    @Enumerated(EnumType.STRING)
-    private Status status;
-    @JsonFormat(pattern = "MM/yy")
-    private YearMonth mesReferencia;
-    @JsonFormat(pattern = "dd/MM/yyyy")
-    private LocalDate dataPagamento;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	@Enumerated(EnumType.STRING)
+	private Status status;
+	@JsonFormat(pattern = "MM/yy")
+	private YearMonth mesReferencia;
+	@JsonFormat(pattern = "dd/MM/yyyy")
+	private LocalDate dataPagamento;
 
-    @ManyToOne
-    @JoinColumn(name = "matricula_id")
-    private Matricula matricula;
+	@ManyToOne
+	@JoinColumn(name = "matricula_id")
+	private Matricula matricula;
 
 }
